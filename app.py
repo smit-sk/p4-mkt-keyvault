e335cd3b58d8027974cd67b81e8062dda01fab7c
smit-sk <patelsmit7008@gmail.com>
2025-04-17 18:00:41 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+app = Flask(__name__)
+
+vault_url = "https://p4keyvault.vault.azure.net/"
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=vault_url, credential=credential)
+
+@app.route("/")
+def home():
+    secret = client.get_secret("P4SampleSecret")
+    return f"Secret: {secret.value}"
+
+if __name__ == "__main__":
+    app.run(debug=False)
