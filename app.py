aa8ca5120952f02c12dbeba105028d0e3f0f3527
Smit Khokhariya <smitkhokhariya@outlook.com>
2025-04-20 01:29:14 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
+# from azure.identity import DefaultAzureCredential
+# from azure.keyvault.secrets import SecretClient
 
 app = Flask(__name__)
 
-vault_url = "https://p4keyvault.vault.azure.net/"
-credential = DefaultAzureCredential()
-client = SecretClient(vault_url=vault_url, credential=credential)
+# vault_url = "https://p4keyvault.vault.azure.net/"
+# credential = DefaultAzureCredential()
+# client = SecretClient(vault_url=vault_url, credential=credential)
+
+# @app.route("/")
+# def home():
+#     secret = client.get_secret("P4SampleSecret")
+#     return f"Secret: {secret.value}"
 
 @app.route("/")
-def home():
-    secret = client.get_secret("P4SampleSecret")
-    return f"Secret: {secret.value}"
-
-@app.route("/test")
 def test():
     return "App is running!"
 
